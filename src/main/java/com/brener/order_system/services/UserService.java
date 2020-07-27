9152471b1fd69ad664db05aa10f503d85a8e954f
Brener <brenerborges@gmail.com>
2020-07-27 13:18:45 -0300

    Exception handling - delete

--- src/main/java/com/brener/order_system/services/UserService.java
+++ src/main/java/com/brener/order_system/services/UserService.java
@@ -2,8 +2,11 @@ package com.brener.order_system.services;
 
 import com.brener.order_system.entities.User;
 import com.brener.order_system.repositories.UserRepository;
+import com.brener.order_system.services.exceptions.DatabaseException;
 import com.brener.order_system.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj){
